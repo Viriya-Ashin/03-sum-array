fb3cf1430c6dabcdc3155072d5b60efd4edb6af5
Tejobhasa <tejobhasa.mm@gmail.com>
2024-05-20 20:59:44 +0630

    first commit

---
+++ sum-array.js
@@ -0,0 +1,15 @@
+// Write code to add all the numbers in `arr` and return the total
+
+var sumArray = function(arr) {
+    var result = 0;
+
+    for (var i = 0; i < arr.length; i++) {
+        var currentNumber = arr [i];
+        result += currentNumber;    
+    }
+
+    return result;
+};
+
+sumArray([3,1,5,6]);
+
